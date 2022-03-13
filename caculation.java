0cc460ebf6bf469d005ee455283288fec0a815f6
jonghyeon Ha <jhjm5658@naver.com>
2022-03-14 00:26:30 +0900

    세번째

---
+++ caculation.java
@@ -0,0 +1,20 @@
+public class caculation {
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 23;
+
+        int sum = num1+num2;
+        int minus = num1-num2;
+        int mul = num1*num2;
+        double div = (double)num1/num2;
+
+        // 결과 출력
+        System.out.println("더하기 : "+ sum);
+        System.out.println("빼기 : "+ minus);
+        System.out.println("곱하기 : "+ mul);
+        System.out.println("나누기 : "+ div);
+
+
+
+    }
+}
