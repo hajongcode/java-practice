c9f16994eb7a317d9a116d2afab71248c8e5c516
jonghyeon Ha <jhjm5658@naver.com>
2022-03-14 00:40:29 +0900

    숫자뒤에 버리기

---
+++ 숫자뒤에자리버리기.java
@@ -0,0 +1,17 @@
+public class 숫자뒤에자리버리기 {
+    public static void main(String[] args) {
+    //int 변수에 985 넣고
+    //결과창에 900 출력
+    int num = 985;
+    int result = 985/100*100;
+
+    System.out.println("결과 : "+result);
+
+    //int 변수에 999넣고 결과로 998 출력하기
+    
+    int num2 = 999;
+    int res = 999/10*10+2;
+    System.out.println("결과2 : "+res);
+
+    }
+}
