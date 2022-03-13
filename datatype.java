7f77b62895192357aca6f97f2b3fdac886418e9b
jonghyeon Ha <jhjm5658@naver.com>
2022-03-13 21:54:10 +0900

    두번째

---
+++ datatype.java
@@ -0,0 +1,38 @@
+public class datatype {
+    public static void main(String[] args) {
+
+        //논리형 true / false
+        boolean a = true;
+        boolean b = false;
+        boolean bool = 5>4;
+        System.out.println(bool);
+
+        //문자형
+        //char에는 문자 하나만 저장된다 ' '
+        char c = 'j';
+        System.out.println(c);
+
+        //문자열
+        String d = "하종현";
+        System.out.println(d);
+
+        //정수
+        byte b1 = 0;
+        short s2 = 2;
+        int i4 = 25;
+        long l8 = 12345;
+        System.out.println(b1);
+        System.out.println(s2);
+        System.out.println(i4);
+        System.out.println(l8);
+
+        //실수
+        float f = 3.1f;
+        double dou =3.1;
+        System.out.println(f);
+        System.out.println(dou);
+
+
+    }
+
+}
