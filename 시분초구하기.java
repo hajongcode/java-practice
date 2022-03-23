05737f230e102b9aba9d8deda9842c1b07641ca4
jonghyeon Ha <jhjm5658@naver.com>
2022-03-23 22:44:33 +0900

    시분초구하기

---
+++ 시분초구하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class 시분초구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("초 입력 : ");
+        int sec = sc.nextInt();
+
+        //입력받은 초를 시간 분 초로 출력하기
+        int hour = sec/3600;
+        int minute = sec%3600/60;
+        int second = sec%60;
+
+        System.out.println("입력된 시간은"+hour+"시"+minute+"분"+second+"초 입니다.");
+
+
+    }
+}
